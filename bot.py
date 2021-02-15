bc05749f75dfd6d1a5a490eabbadfc79aee6264a
BatMuncher69 <79017074+BatMuncher69@users.noreply.github.com>
2021-02-15 13:59:02 +0000

    Added kick, ban and unban commands

--- bot.py
+++ bot.py
@@ -10,6 +10,30 @@ async def on_ready():
 	print('Bot is ready.')
 
 @client.command()
+async def kick(ctx, member : discord.Member, *, reason=None):
+	await member.kick(reason=reason)
+
+@client.command()
+async def ban(ctx, member : discord.Member, *, reason=None):
+	await member.ban(reason=reason)
+
+
+
+@client.command()
+async def unban(ctx, *, member):
+	banned_users = await ctx.guild.bans()
+	member_name, member_discriminator = member.split('#')
+
+	for ban_entry in banned_users:
+		user = ban_entry.user
+
+		if (user.name, user.discriminator) == (member_name, member_discriminator):
+			await ctx.guild.unban(user)
+			await ctx.send(f'Unbanned {user.mention}')
+			return
+
+
+@client.command()
 async def clear(ctx, amount=5):
 	await ctx.channel.purge(limit=amount)
 
