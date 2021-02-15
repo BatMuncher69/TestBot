a588448f2221cfccbe1818bae9040920c87c994f
BatMuncher69 <79017074+BatMuncher69@users.noreply.github.com>
2021-02-15 14:44:17 +0000

    Cog bugs

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord
-import random
+import os
 from discord.ext import commands
+import random
 
 intents = discord.Intents(messages = True, guilds = True, reactions = True, members = True, presences = True)
 client = commands.Bot(command_prefix = '.', intents = intents)
@@ -9,15 +10,31 @@ client = commands.Bot(command_prefix = '.', intents = intents)
 async def on_ready():
 	print('Bot is ready.')
 
+
+@client.command()
+async def load(ctx, extension):
+	client.load_extension(f'cogs.{extension}')
+
+@client.command()
+async def unload(ctx, extension):
+	client.unload_extension(f'cogs.{extension}')
+
+
+for filename in os.listdir('./cogs'):
+	if filename.endswith('.py'):
+		client.load_extension(f'cogs.{filename[:-3]}')
+
+
+
+
 @client.command()
 async def kick(ctx, member : discord.Member, *, reason=None):
 	await member.kick(reason=reason)
 
+
 @client.command()
 async def ban(ctx, member : discord.Member, *, reason=None):
 	await member.ban(reason=reason)
-
-
 
 @client.command()
 async def unban(ctx, *, member):
@@ -33,13 +50,16 @@ async def unban(ctx, *, member):
 			return
 
 
+
 @client.command()
 async def clear(ctx, amount=5):
 	await ctx.channel.purge(limit=amount)
 
+
 @client.command()
 async def ping(ctx):
 	await ctx.send(f'Pong {round(client.latency * 1000)}ms')
+
 
 @client.command(aliases=['8ball'])
 async def _8ball(ctx, *, question):
