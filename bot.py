3bce5d9231dcec80136679cc90597a8b0392c0b9
BatMuncher69 <79017074+BatMuncher69@users.noreply.github.com>
2021-02-15 01:08:03 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import discord
+import random
 from discord.ext import commands
 
 intents = discord.Intents(messages = True, guilds = True, reactions = True, members = True, presences = True)
@@ -14,25 +15,27 @@ async def ping(ctx):
 
 @client.command(aliases=['8ball'])
 async def _8ball(ctx, *, question):
-	responses = ["It is certain.", "It is decidedly so.", "Without a doubt.",
+	responses = ["It is certain.", 
+	"It is decidedly so.", 
+	"Without a doubt.",
 	"Yes - definitely.",
-"You may rely on it.",
-"As I see it, yes.",
-"Most likely.",
-"Outlook good.",
-"Yes.",
-"Signs point to yes.",
-"Reply hazy, try again.",
-"Ask again later.",
-"Better not tell you now.",
-"Cannot predict now.",
-"Concentrate and ask again.",
-"Don't count on it.",
-"My reply is no.",
-"My sources say no.",
-"Outlook not so good.",
-"Very doubtful."]
-
+	"You may rely on it.",
+	"As I see it, yes.",
+	"Most likely.",
+	"Outlook good.",
+	"Yes.",
+	"Signs point to yes.",
+	"Reply hazy, try again.",
+	"Ask again later.",
+	"Better not tell you now.",
+	"Cannot predict now.",
+	"Concentrate and ask again.",
+	"Don't count on it.",
+	"My reply is no.",
+	"My sources say no.",
+	"Outlook not so good.",
+	"Very doubtful."]
+	await ctx.send(f'question:{question}\nAnswer: {random.choice(responses)}')
 
 @client.event
 async def on_member_join(member):
