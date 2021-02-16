7af17f20ab65bceb2801ceaafa4676d00f68fded
AtomicProgramming <georgetimms2005@outlook.com>
2021-02-16 02:07:25 +0000

    reformatted code and added info messages at the end of an event

--- bot.py
+++ bot.py
@@ -3,100 +3,109 @@ import os
 from discord.ext import commands
 import random
 
-intents = discord.Intents(messages = True, guilds = True, reactions = True, members = True, presences = True)
-client = commands.Bot(command_prefix = '.', intents = intents)
+intents = discord.Intents(messages=True, guilds=True, reactions=True, members=True, presences=True)
+client = commands.Bot(command_prefix='.', intents=intents)
+
 
 @client.event
 async def on_ready():
-	print('Bot is ready.')
+    print('Bot is ready.')
 
 
 @client.command()
 async def load(ctx, extension):
-	client.load_extension(f'cogs.{extension}')
+    client.load_extension(f'cogs.{extension}')
+    await ctx.send("Successfully loaded cog {}".format(extension))
+
 
 @client.command()
 async def unload(ctx, extension):
-	client.unload_extension(f'cogs.{extension}')
+    client.unload_extension(f'cogs.{extension}')
+    await ctx.send("Successfully unloaded cog {}".format(extension))
 
 
 for filename in os.listdir('./cogs'):
-	if filename.endswith('.py'):
-		client.load_extension(f'cogs.{filename[:-3]}')
-
-
-
-
-@client.command()
-async def kick(ctx, member : discord.Member, *, reason=None):
-	await member.kick(reason=reason)
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
 
 
 @client.command()
-async def ban(ctx, member : discord.Member, *, reason=None):
-	await member.ban(reason=reason)
+async def kick(ctx, member: discord.Member, *, reason=None):
+    await member.kick(reason=reason)
+    await ctx.send("Successfully kicked {} for reason {}".format(member, reason))
+
+
+@client.command()
+async def ban(ctx, member: discord.Member, *, reason=None):
+    await member.ban(reason=reason)
+    await ctx.send("Successfully banned {} for reason {}".format(member, reason))
+
 
 @client.command()
 async def unban(ctx, *, member):
-	banned_users = await ctx.guild.bans()
-	member_name, member_discriminator = member.split('#')
+    banned_users = await ctx.guild.bans()
+    member_name, member_discriminator = member.split('#')
 
-	for ban_entry in banned_users:
-		user = ban_entry.user
+    for ban_entry in banned_users:
+        user = ban_entry.user
 
-		if (user.name, user.discriminator) == (member_name, member_discriminator):
-			await ctx.guild.unban(user)
-			await ctx.send(f'Unbanned {user.mention}')
-			return
-
+        if (user.name, user.discriminator) == (member_name, member_discriminator):
+            await ctx.guild.unban(user)
+            await ctx.send(f'Unbanned {user.mention}')
+            return
 
 
 @client.command()
 async def clear(ctx, amount=5):
-	await ctx.channel.purge(limit=amount)
+    await ctx.channel.purge(limit=amount)
+
 
 @client.command(aliases=['8ball'])
 async def _8ball(ctx, *, question):
-	responses = ["It is certain.", 
-	"It is decidedly so.", 
-	"Without a doubt.",
-	"Yes - definitely.",
-	"You may rely on it.",
-	"As I see it, yes.",
-	"Most likely.",
-	"Outlook good.",
-	"Yes.",
-	"Signs point to yes.",
-	"Reply hazy, try again.",
-	"Ask again later.",
-	"Better not tell you now.",
-	"Cannot predict now.",
-	"Concentrate and ask again.",
-	"Don't count on it.",
-	"My reply is no.",
-	"My sources say no.",
-	"Outlook not so good.",
-	"Very doubtful."]
-	await ctx.send(f'question: {question}\nAnswer: {random.choice(responses)}')
+    responses = ["It is certain.",
+                 "It is decidedly so.",
+                 "Without a doubt.",
+                 "Yes - definitely.",
+                 "You may rely on it.",
+                 "As I see it, yes.",
+                 "Most likely.",
+                 "Outlook good.",
+                 "Yes.",
+                 "Signs point to yes.",
+                 "Reply hazy, try again.",
+                 "Ask again later.",
+                 "Better not tell you now.",
+                 "Cannot predict now.",
+                 "Concentrate and ask again.",
+                 "Don't count on it.",
+                 "My reply is no.",
+                 "My sources say no.",
+                 "Outlook not so good.",
+                 "Very doubtful."]
+    await ctx.send(f'question: {question}\nAnswer: {random.choice(responses)}')
+
 
 @client.event
 async def on_member_join(member):
-	print(f'{member} has joined a server')
+    print(f'{member} has joined a server')
+
 
 @client.event
 async def on_member_remove(member):
-	print(f'{member} has fucked off.')
+    print(f'{member} has fucked off.')
+
 
 def start_check():
-	import startup
-	for item in dir(startup):
-		item = getattr(startup, item)
-		if callable(item):
-			item()
+    import startup
+    for item in dir(startup):
+        item = getattr(startup, item)
+        if callable(item):
+            item()
+
 
 start_check()
 
 with open("./data/token", "r") as f:
-	token = f.read()
+    token = f.read()
 
 client.run(token)
