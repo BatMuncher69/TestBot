64a70d644d1e81d345e75f3e61cf1b70c54ac95d
BatMuncher69 <79017074+BatMuncher69@users.noreply.github.com>
2021-02-17 01:28:48 +0000

    added reload command
    and test cog

--- bot.py
+++ bot.py
@@ -23,6 +23,12 @@ async def unload(ctx, extension):
     client.unload_extension(f'cogs.{extension}')
     await ctx.send("Successfully unloaded cog {}".format(extension))
 
+@client.command()
+async def reload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+    client.load_extension(f'cogs.{extension}')
+    await ctx.send("Successfully reloaded cog {}".format(extension))
+
 
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
