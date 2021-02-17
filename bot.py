75c90c12b83ea22665d5bf577ab462a0bd34fd63
BatMuncher69 <79017074+BatMuncher69@users.noreply.github.com>
2021-02-17 17:04:11 +0000

    Added bot status

--- bot.py
+++ bot.py
@@ -11,6 +11,7 @@ client = commands.Bot(command_prefix='.', intents=intents)
 
 @client.event
 async def on_ready():
+    await client.change_presence(status=discord.Status.online, activity=discord.Game('Bonjour'))
     print('Bot is ready.')
 
 
