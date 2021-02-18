63d646b1fec9517f93b7ad268bc6537a1555af8f
BatMuncher69 <79017074+BatMuncher69@users.noreply.github.com>
2021-02-18 01:56:52 +0000

    Added minehut api
    .minecraft "server name"

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from itertools import cycle
 import random
 import json
 import youtube_dl
+import requests
 
 DEFAULT_PREFIX = "."
 intents = discord.Intents(messages=True, guilds=True, reactions=True, members=True, presences=True)
@@ -155,6 +156,38 @@ async def unban(ctx, *, member):
             return
 
 
+
+
+
+@client.command()
+async def minecraft(ctx, arg):
+    r = requests.get('https://api.minehut.com/server/' + arg + '?byName=true')
+    json_data = r.json()
+
+    description = json_data["server"]["motd"]
+    online = str(json_data["server"]["online"])
+    playerCount = str(json_data["server"]["playerCount"])
+
+    embed = discord.Embed(
+        title=arg + " Server Info",
+        description='Description: ' + description + '\nOnline: ' + online + '\nOnline Players: ' + playerCount,
+        color=discord.Color.dark_green()
+    )
+    embed.set_thumbnail(url="https://i1.wp.com/www.craftycreations.net/wp-content/uploads/2019/08/Grass-Block-e1566147655539.png?fit=500%2C500&ssl=1")
+
+    await ctx.send(embed=embed)
+
+
+
+
+
+
+
+
+
+
+
+
 @client.command(aliases=['8ball'])
 async def _8ball(ctx, *, question):
     responses = ["It is certain.",
@@ -180,67 +213,67 @@ async def _8ball(ctx, *, question):
     await ctx.send(f'question: {question}\nAnswer: {random.choice(responses)}')
 
 
-@client.command()
-async def play(ctx, url: str):
-    song_there = os.path.isfile("song.mp3")
-    try:
-        if song_there:
-            os.remove("song.mp3")
-    except PermissionError:
-        await ctx.send("Wait for the current playing music to end or use the 'stop' command")
-        return
+# @client.command()
+# async def play(ctx, url: str):
+#     song_there = os.path.isfile("song.mp3")
+#     try:
+#         if song_there:
+#             os.remove("song.mp3")
+#     except PermissionError:
+#         await ctx.send("Wait for the current playing music to end or use the 'stop' command")
+#         return
 
-    voice_channel = discord.utils.get(ctx.guild.voice_channels, name='General')
-    await voice_channel.connect()
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+#     voice_channel = discord.utils.get(ctx.guild.voice_channels, name='Shpek')
+#     await voice_channel.connect()
+#     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
 
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
-        }],
-    }
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
-    for file in os.listdir("./"):
-        if file.endswith(".mp3"):
-            os.rename(file, "song.mp3")
-    voice.play(discord.FFmpegPCMAudio("song.mp3"))
-
-
-@client.command()
-async def leave(ctx):
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    if voice.is_connected():
-        await voice.disconnect()
-    else:
-        await ctx.send("The bot is not connected to a voice channel.")
+#     ydl_opts = {
+#         'format': 'bestaudio/best',
+#         'postprocessors': [{
+#             'key': 'FFmpegExtractAudio',
+#             'preferredcodec': 'mp3',
+#             'preferredquality': '192',
+#         }],
+#     }
+#     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+#         ydl.download([url])
+#     for file in os.listdir("./"):
+#         if file.endswith(".mp3"):
+#             os.rename(file, "song.mp3")
+#     voice.play(discord.FFmpegPCMAudio("song.mp3"))
 
 
-@client.command()
-async def pause(ctx):
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    if voice.is_playing():
-        voice.pause()
-    else:
-        await ctx.send("Currently no audio is playing.")
+# @client.command()
+# async def leave(ctx):
+#     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+#     if voice.is_connected():
+#         await voice.disconnect()
+#     else:
+#         await ctx.send("The bot is not connected to a voice channel.")
 
 
-@client.command()
-async def resume(ctx):
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    if voice.is_paused():
-        voice.resume()
-    else:
-        await ctx.send("The audio is not paused.")
+# @client.command()
+# async def pause(ctx):
+#     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+#     if voice.is_playing():
+#         voice.pause()
+#     else:
+#         await ctx.send("Currently no audio is playing.")
 
 
-@client.command()
-async def stop(ctx):
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    voice.stop()
+# @client.command()
+# async def resume(ctx):
+#     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+#     if voice.is_paused():
+#         voice.resume()
+#     else:
+#         await ctx.send("The audio is not paused.")
+
+
+# @client.command()
+# async def stop(ctx):
+#     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+#     voice.stop()
 
 
 @client.command()
