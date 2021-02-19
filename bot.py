81b6591b88460dfb0de7a0798d60fbbd2497889d
BatMuncher69 <79017074+BatMuncher69@users.noreply.github.com>
2021-02-19 02:16:11 +0000

    broken music
    "Task was destroyed but it is pending!
    
    wait_for=<Future pending cb=[<TaskWakeupMethWrapper object at 0x000001933023C430>()]>>"

--- bot.py
+++ bot.py
@@ -1,11 +1,18 @@
 import discord
 import os
 from discord.ext import commands, tasks
+from discord.voice_client import VoiceClient
 from itertools import cycle
+from random import choice
 import random
 import json
-import youtube_dl
 import requests
+import youtube_dl
+import asyncio
+
+
+
+
 
 DEFAULT_PREFIX = "."
 intents = discord.Intents(messages=True, guilds=True, reactions=True, members=True, presences=True)
@@ -178,16 +185,6 @@ async def minecraft(ctx, arg):
     await ctx.send(embed=embed)
 
 
-
-
-
-
-
-
-
-
-
-
 @client.command(aliases=['8ball'])
 async def _8ball(ctx, *, question):
     responses = ["It is certain.",
@@ -213,67 +210,139 @@ async def _8ball(ctx, *, question):
     await ctx.send(f'question: {question}\nAnswer: {random.choice(responses)}')
 
 
-# @client.command()
-# async def play(ctx, url: str):
-#     song_there = os.path.isfile("song.mp3")
-#     try:
-#         if song_there:
-#             os.remove("song.mp3")
-#     except PermissionError:
-#         await ctx.send("Wait for the current playing music to end or use the 'stop' command")
-#         return
-
-#     voice_channel = discord.utils.get(ctx.guild.voice_channels, name='Shpek')
-#     await voice_channel.connect()
-#     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-
-#     ydl_opts = {
-#         'format': 'bestaudio/best',
-#         'postprocessors': [{
-#             'key': 'FFmpegExtractAudio',
-#             'preferredcodec': 'mp3',
-#             'preferredquality': '192',
-#         }],
-#     }
-#     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-#         ydl.download([url])
-#     for file in os.listdir("./"):
-#         if file.endswith(".mp3"):
-#             os.rename(file, "song.mp3")
-#     voice.play(discord.FFmpegPCMAudio("song.mp3"))
 
 
-# @client.command()
-# async def leave(ctx):
-#     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-#     if voice.is_connected():
-#         await voice.disconnect()
-#     else:
-#         await ctx.send("The bot is not connected to a voice channel.")
 
 
-# @client.command()
-# async def pause(ctx):
-#     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-#     if voice.is_playing():
-#         voice.pause()
-#     else:
-#         await ctx.send("Currently no audio is playing.")
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
 
 
-# @client.command()
-# async def resume(ctx):
-#     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-#     if voice.is_paused():
-#         voice.resume()
-#     else:
-#         await ctx.send("The audio is not paused.")
+queue = []
 
 
-# @client.command()
-# async def stop(ctx):
-#     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-#     voice.stop()
+
+@client.command(name='die', help='This command returns a random last words')
+async def die(ctx):
+    responses = ['why have you brought my short life to an end', 'i could have done so much more', 'i have a family, kill them instead']
+    await ctx.send(choice(responses))
+
+
+
+@client.command(name='join', help='This command makes the bot join the voice channel')
+async def join(ctx):
+    if not ctx.message.author.voice:
+        await ctx.send("You are not connected to a voice channel")
+        return
+    
+    else:
+        channel = ctx.message.author.voice.channel
+
+    await channel.connect()
+
+@client.command(name='queue', help='This command adds a song to the queue')
+async def queue_(ctx, url):
+    global queue
+
+    queue.append(url)
+    await ctx.send(f'`{url}` added to queue!')
+
+@client.command(name='remove', help='This command removes an item from the list')
+async def remove(ctx, number):
+    global queue
+
+    try:
+        del(queue[int(number)])
+        await ctx.send(f'Your queue is now `{queue}!`')
+    
+    except:
+        await ctx.send('Your queue is either **empty** or the index is **out of range**')
+        
+@client.command(name='play', help='This command plays songs')
+async def play(ctx):
+    global queue
+
+    server = ctx.message.guild
+    voice_channel = server.voice_client
+
+    async with ctx.typing():
+        player = await YTDLSource.from_url(queue[0], loop=client.loop)
+        voice_channel.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
+
+    await ctx.send('**Now playing:** {}'.format(player.title))
+    del(queue[0])
+
+@client.command(name='pause', help='This command pauses the song')
+async def pause(ctx):
+    server = ctx.message.guild
+    voice_channel = server.voice_client
+
+    voice_channel.pause()
+
+@client.command(name='resume', help='This command resumes the song!')
+async def resume(ctx):
+    server = ctx.message.guild
+    voice_channel = server.voice_client
+
+    voice_channel.resume()
+
+@client.command(name='view', help='This command shows the queue')
+async def view(ctx):
+    await ctx.send(f'Your queue is now `{queue}!`')
+
+@client.command(name='leave', help='This command stops makes the bot leave the voice channel')
+async def leave(ctx):
+    voice_client = ctx.message.guild.voice_client
+    await voice_client.disconnect()
+
+@client.command(name='stop', help='This command stops the song!')
+async def stop(ctx):
+    server = ctx.message.guild
+    voice_channel = server.voice_client
+
+    voice_channel.stop()
+
+
 
 
 @client.command()
@@ -294,9 +363,6 @@ async def shutdown(ctx):
     await ctx.send("You do not have authorised permissions to use this command!")
 
 
-@client.event
-async def on_member_join(member):
-    print(f'{member} has joined a server')
 
 
 @client.event
