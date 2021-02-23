4de68a71eaf685f39a33f6cd44093bbc9e26a25d
BatMuncher69 <79017074+BatMuncher69@users.noreply.github.com>
2021-02-23 21:37:39 +0000

    custom help command

--- cogs/example.py
+++ cogs/example.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands
 
-
 class Example(commands.Cog):
 
 	def __init__(self, client):
@@ -16,6 +15,16 @@ class Example(commands.Cog):
 	@commands.command()
 	async def ping(self, ctx):
 		await ctx.send('Pong')
+		
+		
+	@commands.command(invoke_without_command=True)
+	async def helpp(self, ctx):
+		embed = discord.Embed(title = "Help", description = "use .help <command> for more info on a command.", color = ctx.author.color())
+		embed.add_field(name = "moderation", value = "clear, kick, ban, whois")
+		embed.add_field(name = "fun", value = "8ball" )
+		embed.add_field(name = "random", value = "minecraft, hello")
+		await ctx(embed=embed)
+
 
 
 def setup(client):
